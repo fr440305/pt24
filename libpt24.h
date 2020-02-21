05ada3872959b0daf463b14b2bea8255d281ce55
fr440305 <fr440305@gmail.com>
2020-02-21 01:25:31 -0800

    Anyway

--- libpt24.h
+++ libpt24.h
@@ -9,7 +9,33 @@
 
 #define LIBPT24_VERSION "0.0.1"
 
-/* if no solution, return 0. if buffer overflows, return -1. if success, return 1. */
+/* libpt24_find_solution
+//
+//    - Given 4 long (64-bit) numbers, find the expression.
+//
+//PARAMETERS
+//
+//    a        : An array of 4 long numbers.
+//    buf      : Buffer. The function will
+//               write the string representation
+//               of the expression into it.
+//    capacity : The capacity of the buffer.
+//    ofmt     : Output format. Be it the macros
+//               that starts with ``OFMT_''.
+//
+//RETURN
+//
+//    0 : if no expression of these 4 numbers can be 24.
+//    1 : Success.
+//    -1: Buffer overflow. Larger buffer needed.
+//
+//EXAMPLE
+//
+//    long nums[4] = {1, 2, 3, 4};
+//    char buf[512] = {0};
+//    libpt24_find_solution(nums, buf, 512, OFMT_PARENTHESIS);
+//    printf("%s\n", buf);
+*/
 int libpt24_find_solution(
 	long a[4],
 	char *buf,
--- libpt24.js
+++ libpt24.js
@@ -1,13 +1,94 @@
 
-function Fraction() {
-	this.n = 0;
-	this.d = 1;
+function Expr(a, b, c, d) {
+	this.types = [
+		"fxfxfxx",
+		"fffxxxx",
+		"ffxxfxx",
+		"fxxffxx",
+		""
+	];
+	this.currentTypeIndex = 0;
+
+	this.fs = [];
+
+	var operators = ["+", "-", "*", "/"];
+	for (var i = 0; i < 4; i++) {
+		for (var j = 0; j < 4; j++) {
+			for (var k = 0; k < 4; k++) {
+				this.fs.push(operators[i] + operators[j] + operators[k])
+			}
+		}
+	}
+
+	this.currentFIndex = 0;
+	this.xarr = [a, b, c, d];
 }
 
-Fraction.prototype['+'] = function(that) {
+Expr.prototype.shiftType = function() {
+	if (this.currentTypeIndex == this.types.length) {
+		return false;
+	}
+
+	this.currentTypeIndex += 1;
+
+	return true;
 }
 
-/* === libpt24 */
-function libpt24FindSolution(a[4], ofmt) {
-	
+Expr.prototype.reset = function(a, b, c, d) {
+	this.currentTypeIndex = 0;
+	this.currentFIndex = 0;
+	this.x = [a, b, c, d];
+}
+
+Expr.prototype.shiftOperators = function() {
+	if (this.currentFIndex == this.fs.length) {
+		return false; // not shiftable
+	}
+
+	this.currentFIndex += 1;
+
+	return true;
+}
+
+Expr.prototype.calc = function() {
+	var stack = [];
+	var currentType = this.types[this.currentTypeIndex];
+	var operators = this.fs[this.currentFIndex];
+	var f = 3;
+	var x = 4;
+
+	for (var i = 6; i >= 0; i--) {
+		switch (currentType[i]) {
+		case 'x':
+			stack.push(this.x[x]);
+			break;
+		case 'f':
+			var r = stack.pop();
+			var l = stack.pop();
+			stack.push(({ // todo : use if-else.
+				"+": l + r,
+				"-": l - r,
+				"*": l * r,
+				"/": l / r
+			})[operators[f]]);
+		}
+	}
+}
+
+
+Expr.prototype.toString = function(fmt) {
+}
+
+function calculateTree(tree) {
+	// {}
+}
+
+function findExpr(a, b, c, d) {
+	var e = new Expr(a, b, c, d);
+	while () {
+		while () {
+			while () {
+			}
+		}
+	}
 }
